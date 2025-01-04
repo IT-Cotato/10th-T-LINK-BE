cf70b9b6063c5d4183d1831bc4041570e433eb15
chyun <och2510@gmail.com>
2025-01-04 22:05:38 +0900

    Init [#1] 기초 프로젝트 세팅

---
+++ src/main/java/org/cotato/tlinkserver/TlinkServerApplication.java
@@ -0,0 +1,13 @@
+package org.cotato.tlinkserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TlinkServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TlinkServerApplication.class, args);
+    }
+
+}
