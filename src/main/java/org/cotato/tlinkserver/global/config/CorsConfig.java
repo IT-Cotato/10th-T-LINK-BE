9250a7dc937c7fe47d0f518c5453ec166c175b50
chyun <och2510@gmail.com>
2025-01-04 23:19:14 +0900

    File [#5] 기본 Security 및 CORS 설정

---
+++ src/main/java/org/cotato/tlinkserver/global/config/CorsConfig.java
@@ -0,0 +1,45 @@
+package org.cotato.tlinkserver.global.config;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class CorsConfig {
+
+    public static CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        //리소스를 허용할 URL 지정
+        ArrayList<String> allowedOriginPatterns = new ArrayList<>();
+        allowedOriginPatterns.add("http://localhost:3000");
+        allowedOriginPatterns.add("http://127.0.0.1:3000");
+        allowedOriginPatterns.add("https://t-link.site");
+        allowedOriginPatterns.add("https://api.t-link.site");
+        configuration.setAllowedOrigins(allowedOriginPatterns);
+
+        //허용하는 HTTP METHOD 지정
+        ArrayList<String> allowedHttpMethods = new ArrayList<>();
+        allowedHttpMethods.add("GET");
+        allowedHttpMethods.add("POST");
+        allowedHttpMethods.add("PUT");
+        allowedHttpMethods.add("DELETE");
+        allowedHttpMethods.add("OPTIONS");
+        configuration.setAllowedMethods(allowedHttpMethods);
+
+        configuration.setAllowedHeaders(Collections.singletonList("*"));
+//        configuration.setAllowedHeaders(List.of(HttpHeaders.AUTHORIZATION, HttpHeaders.CONTENT_TYPE));
+
+        //인증, 인가를 위한 credentials 를 TRUE로 설정
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+}
