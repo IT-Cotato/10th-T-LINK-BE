1cb6519b284c178d2cea1be621fa5bb8b9e1f99d
chyun <och2510@gmail.com>
2025-01-04 22:05:38 +0900

    Init [#1] 기초 프로젝트 세팅

---
+++ src/test/java/org/cotato/tlinkserver/TlinkServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cotato.tlinkserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TlinkServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
