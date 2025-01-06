f70f8dc06b865babd6d94ace8f0f4928ef864ad3
chyun <och2510@gmail.com>
2025-01-07 01:03:27 +0900

    Release [#20] CD 파일 cloud yml 경로 추가

--- src/main/java/org/cotato/tlinkserver/global/util/S3FileHandler.java
+++ src/main/java/org/cotato/tlinkserver/global/util/S3FileHandler.java
@@ -14,12 +14,14 @@ import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
+@PropertySource("classpath:cloud/application-cloud-profile.yml")
 public class S3FileHandler {
 
     private final AmazonS3Client amazonS3Client;
