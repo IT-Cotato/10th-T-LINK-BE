f5727554f033560ca29bcb04261bbf06e67be811
Yeongchan Kim <hades1@kakao.com>
2025-01-09 00:07:40 +0900

    Refactor [#31] 권한 엔티티 강의 자료 명칭 수정

---
+++ src/main/java/org/cotato/tlinkserver/domain/room/RoomPermission.java
@@ -0,0 +1,42 @@
+package org.cotato.tlinkserver.domain.room;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Setter
+public class RoomPermission {
+
+	@Column(name = "lec_files")
+	private boolean lecFiles;
+
+	@Column(name = "homework")
+	private boolean homework;
+
+	@Column(name = "grade_statistic")
+	private boolean gradeStatistic;
+
+	@Column(name = "counseling_log")
+	private boolean counselingLog;
+
+	@Column(name = "account")
+	private boolean account;
+
+	@Builder
+	public RoomPermission(boolean lecFiles, boolean homework, boolean gradeStatistic, boolean counselingLog,
+		boolean account) {
+		this.lecFiles = lecFiles;
+		this.homework = homework;
+		this.gradeStatistic = gradeStatistic;
+		this.counselingLog = counselingLog;
+		this.account = account;
+	}
+}
