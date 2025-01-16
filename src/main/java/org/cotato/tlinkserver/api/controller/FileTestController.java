cedd8397ba620fde98bacfb1e16c280465e83dce
Yeongchan Kim <hades1@kakao.com>
2025-01-16 23:40:11 +0900

    Refactor [#39] 파일 관련 테스트 컨트롤러

--- src/main/java/org/cotato/tlinkserver/api/controller/FileTestController.java
+++ src/main/java/org/cotato/tlinkserver/api/controller/FileTestController.java
@@ -1,32 +1,49 @@
 package org.cotato.tlinkserver.api.controller;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
+
 import org.cotato.tlinkserver.global.util.S3FileHandler;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import io.awspring.cloud.s3.S3Resource;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/file")
 public class FileTestController {
 
-    private final S3FileHandler s3FileHandler;
+	private final S3FileHandler s3FileHandler;
 
-    @PostMapping
-    public ResponseEntity<List<String>> uploadFiles(List<MultipartFile> multipartFiles) throws IOException {
-        return ResponseEntity.ok(s3FileHandler.examples(multipartFiles, 1L));
-    }
+	@PostMapping("/upload")
+	public ResponseEntity<?> uploadFiles(@RequestPart(value = "file") List<MultipartFile> multipartFiles) throws
+		IOException {
+		List<String> list = Arrays.asList("aa", "bb", "cc");
+		s3FileHandler.uploadFiles(multipartFiles, list);
+		return ResponseEntity.ok("success");
+	}
 
-    @DeleteMapping
-    public ResponseEntity<List<String>> deleteFiles(@RequestBody List<String> fileUrls) {
-        s3FileHandler.deleteFiles(fileUrls);
-        return ResponseEntity.ok(fileUrls);
-    }
+	@PostMapping("/download")
+	public ResponseEntity<?> downloadFile() {
+		S3Resource aa = s3FileHandler.downloadFile("aa");
+		return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(aa);
+	}
+
+	@DeleteMapping("/delete")
+	public void deleteFile() {
+		List<String> list = Arrays.asList("aa", "bb");
+		s3FileHandler.deleteFiles(list);
+	}
+
 }
+
+
