de7f97556410c39ccfedac21238361c27a3db599
Yeongchan Kim <hades1@kakao.com>
2025-01-16 21:27:54 +0900

    Refactor [#39] 파일 업로드

--- src/main/java/org/cotato/tlinkserver/global/util/S3FileHandler.java
+++ src/main/java/org/cotato/tlinkserver/global/util/S3FileHandler.java
@@ -1,20 +1,31 @@
 package org.cotato.tlinkserver.global.util;
 
-import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
+import io.awspring.cloud.s3.ObjectMetadata;
+import io.awspring.cloud.s3.S3Operations;
+import io.awspring.cloud.s3.S3Resource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import software.amazon.awssdk.services.s3.model.NoSuchKeyException;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
@@ -22,81 +33,17 @@ import org.springframework.web.multipart.MultipartFile;
 @Slf4j
 public class S3FileHandler {
 
-    private final AmazonS3Client amazonS3Client;
+    private final S3Operations s3Operations;
 
-    @Value("${cloud.aws.s3.bucket-name}")
+    @Value("${spring.cloud.aws.s3.bucket-name}")
     private String bucket;
 
-    private final String EXAMPLE_DIR = "example/";
-
-    public List<String> examples(final List<MultipartFile> multipartFiles, final Long userId) throws IOException {
-        List<String> exampleUploadUrls = new ArrayList<>();
-
-        multipartFiles.forEach(mf -> {
-            try {
-                exampleUploadUrls.add(example(mf, userId));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
-
-        return exampleUploadUrls;
-    }
-
-    public String example(final MultipartFile multipartFile, final Long userId) throws IOException {
-        File uploadFile = convert(multipartFile)
-                .orElseThrow(() -> new IllegalArgumentException("[ERROR] MultipartFile -> File convert fail"));
-
-        return upload(uploadFile, EXAMPLE_DIR, userId);
-    }
-
-    // S3 파일 삭제
-    public void deleteFile(final String fileUrl) {
-        amazonS3Client.deleteObject(new DeleteObjectRequest(bucket, fileUrl));
-    }
-
-    public void deleteFiles(final List<String> fileUrls) {
-        fileUrls.forEach(this::deleteFile);
-    }
-
-    // S3로 파일 업로드
-    private String upload(final File uploadFile, final String dirName, final Long userId) {
-        String fileName = dirName + userId + "/" + UUID.randomUUID() + uploadFile.getName();
-
-        String uploadFileUrl = putS3(uploadFile, fileName);
-        removeNewFile(uploadFile);
-
-        return uploadFileUrl;
-    }
-
-    // S3로 업로드
-    private String putS3(final File uploadFile, final String fileName) {
-        amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, uploadFile).withCannedAcl(
-                CannedAccessControlList.PublicRead));
-        return amazonS3Client.getUrl(bucket, fileName).toString();
-    }
-
-    // 로컬에 저장된 이미지 지우기
-    private void removeNewFile(final File targetFile) {
-        if (targetFile.delete()) {
-            log.info("File delete success");
-            return;
+    // S3 파일 업로드
+    public void upload(final MultipartFile multipartFile, final String key) throws IOException {
+        try (InputStream is = multipartFile.getInputStream()) {
+			s3Operations.upload(bucket, key, is,
+                ObjectMetadata.builder().contentType(multipartFile.getContentType()).build());
         }
-
-        log.info("File delete fail: " + targetFile.getPath());
     }
 
-    // 로컬에 파일 업로드 하기
-    private Optional<File> convert(final MultipartFile file) throws IOException {
-        String filePath = System.getProperty("user.home") + "/" + file.getOriginalFilename();
-
-        File convertFile = new File(filePath);
-        if (convertFile.createNewFile()) { // 바로 위에서 지정한 경로에 File이 생성됨
-            try (FileOutputStream fos = new FileOutputStream(convertFile)) {
-                fos.write(file.getBytes());
-            }
-            return Optional.of(convertFile);
-        }
-        return Optional.empty();
-    }
 }
