517c41aa2ef10c2b6d469b2ff1eefef80a57948d
Yeongchan Kim <hades1@kakao.com>
2025-01-17 21:16:47 +0900

    Feat [#41] 강의 자료 목록 조회 구현

--- src/main/java/org/cotato/tlinkserver/api/controller/LectureFileBoxController.java
+++ src/main/java/org/cotato/tlinkserver/api/controller/LectureFileBoxController.java
@@ -1,10 +1,29 @@
 package org.cotato.tlinkserver.api.controller;
 
+import org.cotato.tlinkserver.api.dto.response.DataResponse;
+import org.cotato.tlinkserver.api.facade.LectureFileBoxFacade;
+import org.cotato.tlinkserver.domain.lectureFile.application.dto.response.LectureFileBoxesDTO;
+import org.cotato.tlinkserver.global.util.SuccessMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/api/v1/rooms/{roomId}/lectureFileBoxes")
+@RequiredArgsConstructor
 public class LectureFileBoxController {
+
+	private final LectureFileBoxFacade lectureFileBoxFacade;
+
+	@GetMapping
+	public ResponseEntity<DataResponse<LectureFileBoxesDTO>> getLectureFileBoxes(@PathVariable(value = "roomId") Long roomId) {
+		LectureFileBoxesDTO lectureFileBoxes = lectureFileBoxFacade.getLectureFileBoxes(roomId);
+		return ResponseEntity.ok(DataResponse.of(HttpStatus.OK, SuccessMessage.SUCCESS.getDetailMessage(), lectureFileBoxes));
+	}
 
 }
--- src/main/java/org/cotato/tlinkserver/domain/lectureFile/application/LectureFileBoxService.java
+++ src/main/java/org/cotato/tlinkserver/domain/lectureFile/application/LectureFileBoxService.java
@@ -1,5 +1,9 @@
 package org.cotato.tlinkserver.domain.lectureFile.application;
 
+import java.util.List;
+
+import org.cotato.tlinkserver.domain.lectureFile.LectureFileBox;
+import org.cotato.tlinkserver.domain.lectureFile.application.dto.response.LectureFileBoxesDTO;
 import org.cotato.tlinkserver.domain.lectureFile.infra.repository.LectureFileBoxRepository;
 import org.springframework.stereotype.Service;
 
@@ -13,5 +17,10 @@ import lombok.extern.slf4j.Slf4j;
 public class LectureFileBoxService {
 
 	private final LectureFileBoxRepository lectureFileBoxRepository;
+
+	public LectureFileBoxesDTO getLectureFileBoxes(final Long roomId) {
+		List<LectureFileBox> lectureFileBoxes = lectureFileBoxRepository.findLectureFileBoxesByRoomId(roomId);
+		return LectureFileBoxesDTO.from(lectureFileBoxes);
+	}
 
 }
