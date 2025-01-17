81eabd7dd602b2da03492a6d6685eed2ddd0484e
Yeongchan Kim <hades1@kakao.com>
2025-01-17 16:14:23 +0900

    Feat [#41] 강의 자료함 관련 Repository 생성

---
+++ src/main/java/org/cotato/tlinkserver/domain/lectureFile/infra/repository/LectureFileBoxRepository.java
@@ -0,0 +1,7 @@
+package org.cotato.tlinkserver.domain.lectureFile.infra.repository;
+
+import org.cotato.tlinkserver.domain.lectureFile.LectureFileBox;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LectureFileBoxRepository extends JpaRepository<LectureFileBox, Long> {
+}
