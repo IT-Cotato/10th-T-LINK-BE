444378ec33f0b76eae8cca810953320dbc25c3ae
Yeongchan Kim <hades1@kakao.com>
2025-01-17 21:16:47 +0900

    Feat [#41] 강의 자료 목록 조회 구현

--- src/main/java/org/cotato/tlinkserver/api/facade/LectureFileBoxFacade.java
+++ src/main/java/org/cotato/tlinkserver/api/facade/LectureFileBoxFacade.java
@@ -2,6 +2,7 @@ package org.cotato.tlinkserver.api.facade;
 
 import org.cotato.tlinkserver.domain.lectureFile.application.LectureFileBoxService;
 import org.cotato.tlinkserver.domain.lectureFile.application.LectureFileService;
+import org.cotato.tlinkserver.domain.lectureFile.application.dto.response.LectureFileBoxesDTO;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
@@ -12,5 +13,9 @@ public class LectureFileBoxFacade {
 
 	private final LectureFileBoxService lectureFileBoxService;
 	private final LectureFileService lectureFileService;
+
+	public LectureFileBoxesDTO getLectureFileBoxes(final Long roomId) {
+		return lectureFileBoxService.getLectureFileBoxes(roomId);
+	}
 
 }
