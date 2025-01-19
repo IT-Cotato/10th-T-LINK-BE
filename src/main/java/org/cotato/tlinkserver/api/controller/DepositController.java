0972fa9653fac847e4a1e682b198659966ba41f1
Yeongchan Kim <hades1@kakao.com>
2025-01-19 22:30:06 +0900

    Feat [#45] 입금 상세 조회 구현

--- src/main/java/org/cotato/tlinkserver/api/controller/DepositController.java
+++ src/main/java/org/cotato/tlinkserver/api/controller/DepositController.java
@@ -1,6 +1,14 @@
 package org.cotato.tlinkserver.api.controller;
 
+import org.cotato.tlinkserver.api.dto.response.DataResponse;
 import org.cotato.tlinkserver.api.facade.DepositFacade;
+import org.cotato.tlinkserver.domain.bank.application.dto.response.DepositResponse;
+import org.cotato.tlinkserver.domain.lectureFile.application.dto.response.LectureFileBoxDetailResponse;
+import org.cotato.tlinkserver.global.util.SuccessMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +20,11 @@ import lombok.RequiredArgsConstructor;
 public class DepositController {
 
 	private final DepositFacade depositFacade;
+
+	@GetMapping
+	public ResponseEntity<DataResponse<DepositResponse>> getDeposit(@PathVariable(value = "roomId") Long roomId) {
+		DepositResponse deposit = depositFacade.getDeposit(roomId);
+		return ResponseEntity.ok(DataResponse.of(HttpStatus.OK, SuccessMessage.SUCCESS.getDetailMessage(), deposit));
+	}
 
 }
