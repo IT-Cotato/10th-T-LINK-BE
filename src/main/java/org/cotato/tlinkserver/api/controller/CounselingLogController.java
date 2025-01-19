814908d4b1aee5184cef3093000996cfc9449663
Yeongchan Kim <hades1@kakao.com>
2025-01-19 21:54:55 +0900

    Feat [#44] 상담 일지 생성 구현

--- src/main/java/org/cotato/tlinkserver/api/controller/CounselingLogController.java
+++ src/main/java/org/cotato/tlinkserver/api/controller/CounselingLogController.java
@@ -2,6 +2,7 @@ package org.cotato.tlinkserver.api.controller;
 
 import org.cotato.tlinkserver.api.dto.response.DataResponse;
 import org.cotato.tlinkserver.api.facade.CounselingLogFacade;
+import org.cotato.tlinkserver.domain.counselingLog.application.dto.request.CounselingLogSaveRequest;
 import org.cotato.tlinkserver.domain.counselingLog.application.dto.response.CounselingLogDetailResponse;
 import org.cotato.tlinkserver.domain.counselingLog.application.dto.response.CounselingLogsResponse;
 import org.cotato.tlinkserver.global.util.SuccessMessage;
@@ -9,6 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +28,13 @@ public class CounselingLogController {
 	public ResponseEntity<DataResponse<CounselingLogsResponse>> getCounselingLogs(@PathVariable(value = "roomId") Long roomId) {
 		CounselingLogsResponse counselingLogs = counselingLogFacade.getCounselingLogs(roomId);
 		return ResponseEntity.ok(DataResponse.of(HttpStatus.OK, SuccessMessage.SUCCESS.getDetailMessage(), counselingLogs));
+	}
+
+	@PostMapping
+	public ResponseEntity<DataResponse<?>> saveCounselingLog(@PathVariable(value = "roomId") Long roomId,
+		@RequestBody CounselingLogSaveRequest counselingLogSaveRequest) {
+		counselingLogFacade.saveCounselingLog(roomId, counselingLogSaveRequest);
+		return ResponseEntity.ok(DataResponse.of(HttpStatus.CREATED, SuccessMessage.CREATED.getDetailMessage(), null));
 	}
 
 	@GetMapping("/{counselingLogId}")
