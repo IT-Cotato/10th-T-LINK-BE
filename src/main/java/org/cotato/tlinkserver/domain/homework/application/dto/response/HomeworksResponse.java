e29258d7829c2201eae27b33f4fc3771089cc68c
Yeongchan Kim <hades1@kakao.com>
2025-01-19 15:04:30 +0900

    Feat [#43] 숙제 목록 조회 구현

---
+++ src/main/java/org/cotato/tlinkserver/domain/homework/application/dto/response/HomeworksResponse.java
@@ -0,0 +1,18 @@
+package org.cotato.tlinkserver.domain.homework.application.dto.response;
+
+import java.util.List;
+
+import org.cotato.tlinkserver.domain.homework.Homework;
+
+public record HomeworksResponse(
+	List<HomeworkResponse> homeworks
+)
+{
+	public static HomeworksResponse from(List<Homework> homeworks) {
+		return new HomeworksResponse(
+			homeworks.stream()
+				.map(HomeworkResponse::from)
+				.toList()
+		);
+	}
+}
