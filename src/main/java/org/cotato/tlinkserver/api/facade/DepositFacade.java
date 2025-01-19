f1fe7e4f0ae4fd0aff8bf203f4ca4c6a7f6cb316
Yeongchan Kim <hades1@kakao.com>
2025-01-19 22:46:41 +0900

    Feat [#45] Transactional 어노테이션 추가

--- src/main/java/org/cotato/tlinkserver/api/facade/DepositFacade.java
+++ src/main/java/org/cotato/tlinkserver/api/facade/DepositFacade.java
@@ -6,6 +6,7 @@ import org.cotato.tlinkserver.domain.bank.application.dto.response.DepositRespon
 import org.cotato.tlinkserver.domain.room.Room;
 import org.cotato.tlinkserver.domain.room.application.RoomService;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,16 +16,19 @@ public class DepositFacade {
 
 	private final RoomService roomService;
 
+	@Transactional(readOnly = true)
 	public DepositResponse getDeposit(final Long roomId) {
 		Room room = roomService.getRoom(roomId);
 		return DepositResponse.from(room);
 	}
 
+	@Transactional(readOnly = true)
 	public DepositModifyResponse getDepositModify(final Long roomId) {
 		Room room = roomService.getRoom(roomId);
 		return DepositModifyResponse.from(room);
 	}
 
+	@Transactional
 	public void modifyDeposit(final Long roomId, final DepositRequest depositRequest) {
 		Room room = roomService.getRoom(roomId);
 		room.setAccountNumber(depositRequest.accountNumber());
