d0ff2ca17b224dd20749cb7edbd0cca1e659e2fb
Yeongchan Kim <hades1@kakao.com>
2025-01-19 21:36:05 +0900

    Feat [#44] 상담 일지 상세 조회 구현

--- src/main/java/org/cotato/tlinkserver/api/facade/CounselingLogFacade.java
+++ src/main/java/org/cotato/tlinkserver/api/facade/CounselingLogFacade.java
@@ -1,9 +1,11 @@
 package org.cotato.tlinkserver.api.facade;
 
 import org.cotato.tlinkserver.domain.counselingLog.application.dto.CounselingLogService;
+import org.cotato.tlinkserver.domain.counselingLog.application.dto.response.CounselingLogDetailResponse;
 import org.cotato.tlinkserver.domain.counselingLog.application.dto.response.CounselingLogsResponse;
 import org.cotato.tlinkserver.global.util.S3FileHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,6 +16,12 @@ public class CounselingLogFacade {
 	private final CounselingLogService counselingLogService;
 	private final S3FileHandler s3FileHandler;
 
+	@Transactional(readOnly = true)
+	public CounselingLogDetailResponse getCounselingLog(final Long counselingLogId) {
+		return counselingLogService.getCounselingLog(counselingLogId);
+	}
+
+	@Transactional(readOnly = true)
 	public CounselingLogsResponse getCounselingLogs(final Long roomId) {
 		return counselingLogService.getCounselingLogs(roomId);
 	}
