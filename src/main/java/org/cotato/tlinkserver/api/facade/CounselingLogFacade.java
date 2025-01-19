3420453084674a6c7d24264decb49c6a453d4026
Yeongchan Kim <hades1@kakao.com>
2025-01-19 21:21:47 +0900

    Feat [#44] 상담 일지 목록 조회 구현

--- src/main/java/org/cotato/tlinkserver/api/facade/CounselingLogFacade.java
+++ src/main/java/org/cotato/tlinkserver/api/facade/CounselingLogFacade.java
@@ -1,5 +1,10 @@
 package org.cotato.tlinkserver.api.facade;
 
+import java.util.List;
+
+import org.cotato.tlinkserver.domain.counselingLog.application.dto.CounselingLogService;
+import org.cotato.tlinkserver.domain.counselingLog.application.dto.response.CounselingLogResponse;
+import org.cotato.tlinkserver.domain.counselingLog.application.dto.response.CounselingLogsResponse;
 import org.cotato.tlinkserver.global.util.S3FileHandler;
 import org.springframework.stereotype.Component;
 
@@ -9,6 +14,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CounselingLogFacade {
 
+	private final CounselingLogService counselingLogService;
 	private final S3FileHandler s3FileHandler;
 
+	public CounselingLogsResponse getCounselingLogs(Long roomId) {
+		return counselingLogService.getCounselingLogs(roomId);
+	}
 }
--- src/main/java/org/cotato/tlinkserver/domain/counselingLog/infra/repository/CounselingLogRepository.java
+++ src/main/java/org/cotato/tlinkserver/domain/counselingLog/infra/repository/CounselingLogRepository.java
@@ -1,7 +1,15 @@
 package org.cotato.tlinkserver.domain.counselingLog.infra.repository;
 
+import java.util.List;
+
 import org.cotato.tlinkserver.domain.counselingLog.CounselingLog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CounselingLogRepository extends JpaRepository<CounselingLog, Long> {
+
+	@Query("SELECT cl FROM CounselingLog cl WHERE cl.room.id = :roomId")
+	List<CounselingLog> findCounselingLogsByRoomId(@Param("roomId") Long roomId);
+
 }
