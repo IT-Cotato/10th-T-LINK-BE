c57bdd8e4a9e4c0ed079f43f25d13d06bc2d3f93
Yeongchan Kim <hades1@kakao.com>
2025-01-19 21:21:47 +0900

    Feat [#44] 상담 일지 목록 조회 구현

--- src/main/java/org/cotato/tlinkserver/api/controller/CounselingLogController.java
+++ src/main/java/org/cotato/tlinkserver/api/controller/CounselingLogController.java
@@ -1,6 +1,16 @@
 package org.cotato.tlinkserver.api.controller;
 
+import java.util.List;
+
+import org.cotato.tlinkserver.api.dto.response.DataResponse;
 import org.cotato.tlinkserver.api.facade.CounselingLogFacade;
+import org.cotato.tlinkserver.domain.counselingLog.application.dto.response.CounselingLogResponse;
+import org.cotato.tlinkserver.domain.counselingLog.application.dto.response.CounselingLogsResponse;
+import org.cotato.tlinkserver.global.util.SuccessMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +22,11 @@ import lombok.RequiredArgsConstructor;
 public class CounselingLogController {
 
 	private final CounselingLogFacade counselingLogFacade;
+
+	@GetMapping
+	public ResponseEntity<DataResponse<CounselingLogsResponse>> getCounselingLogs(@PathVariable(value = "roomId") Long roomId) {
+		CounselingLogsResponse counselingLogs = counselingLogFacade.getCounselingLogs(roomId);
+		return ResponseEntity.ok(DataResponse.of(HttpStatus.OK, SuccessMessage.SUCCESS.getDetailMessage(), counselingLogs));
+	}
 
 }
