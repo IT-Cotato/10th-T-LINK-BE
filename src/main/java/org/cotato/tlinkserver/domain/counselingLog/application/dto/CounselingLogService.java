156f570661640f6f85ac199e49113247d93dc61c
Yeongchan Kim <hades1@kakao.com>
2025-01-19 22:06:44 +0900

    Feat [#44] 상담 일지 수정 구현

--- src/main/java/org/cotato/tlinkserver/domain/counselingLog/application/dto/CounselingLogService.java
+++ src/main/java/org/cotato/tlinkserver/domain/counselingLog/application/dto/CounselingLogService.java
@@ -3,9 +3,11 @@ package org.cotato.tlinkserver.domain.counselingLog.application.dto;
 import java.util.List;
 
 import org.cotato.tlinkserver.domain.counselingLog.CounselingLog;
+import org.cotato.tlinkserver.domain.counselingLog.application.dto.request.CounselingLogSaveRequest;
 import org.cotato.tlinkserver.domain.counselingLog.application.dto.response.CounselingLogDetailResponse;
 import org.cotato.tlinkserver.domain.counselingLog.application.dto.response.CounselingLogResponse;
 import org.cotato.tlinkserver.domain.counselingLog.application.dto.response.CounselingLogsResponse;
+import org.cotato.tlinkserver.domain.counselingLog.constant.Engagement;
 import org.cotato.tlinkserver.domain.counselingLog.infra.repository.CounselingLogRepository;
 import org.springframework.stereotype.Service;
 
@@ -30,4 +32,13 @@ public class CounselingLogService {
 			.map(CounselingLogResponse::from).toList();
 		return CounselingLogsResponse.from(counselingLogs);
 	}
+
+	public void modifyCounselingLog(final Long counselingLogId, final CounselingLogSaveRequest counselingLogSaveRequest) {
+		CounselingLog counselingLog = counselingLogRepository.findById(counselingLogId).orElseThrow();
+		counselingLog.setTitle(counselingLogSaveRequest.title());
+		counselingLog.setContent(counselingLogSaveRequest.content());
+		counselingLog.setEngagement(Engagement.valueOf(counselingLogSaveRequest.engagement()));
+		counselingLog.setHomeworkSubmitted(counselingLogSaveRequest.homeworkSubmitted());
+	}
+
 }
