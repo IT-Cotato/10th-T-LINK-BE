f3e34fcab575956b15aec0b1cdf12c604b31986e
chyun <och2510@gmail.com>
2025-01-30 01:10:04 +0900

    feat [#48] 유저 레포지토리 생성

---
+++ src/main/java/org/cotato/tlinkserver/domain/user/infra/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package org.cotato.tlinkserver.domain.user.infra.repository;
+
+import org.cotato.tlinkserver.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
