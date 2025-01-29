2b41e2165557f68dfcb1f41ee65d76d2e69943bb
chyun <och2510@gmail.com>
2025-01-30 00:50:29 +0900

    feat [#48] Enum Json 매핑 함수 작성

--- src/main/java/org/cotato/tlinkserver/domain/user/constant/Gender.java
+++ src/main/java/org/cotato/tlinkserver/domain/user/constant/Gender.java
@@ -1,6 +1,10 @@
 package org.cotato.tlinkserver.domain.user.constant;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import java.util.Arrays;
 import lombok.Getter;
+import org.cotato.tlinkserver.global.exception.TLinkException;
+import org.cotato.tlinkserver.global.message.ErrorMessage;
 
 @Getter
 public enum Gender {
@@ -11,5 +15,15 @@ public enum Gender {
 
 	Gender(String inKorean) {
 		this.inKorean = inKorean;
+	}
+
+	@JsonCreator
+	public static Gender from(final String input) {
+		return Arrays.stream(Gender.values())
+				.filter(role -> role.inKorean.equals(input))
+				.findFirst()
+				.orElseThrow(
+						() -> new TLinkException(ErrorMessage.BAD_REQUEST)
+				);
 	}
 }
