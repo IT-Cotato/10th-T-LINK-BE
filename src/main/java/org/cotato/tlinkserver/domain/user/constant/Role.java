ab4b873a9b1e9fa82cb67ddf7bf39831de7fcffc
chyun <och2510@gmail.com>
2025-01-30 00:50:29 +0900

    feat [#48] Enum Json 매핑 함수 작성

--- src/main/java/org/cotato/tlinkserver/domain/user/constant/Role.java
+++ src/main/java/org/cotato/tlinkserver/domain/user/constant/Role.java
@@ -1,6 +1,10 @@
 package org.cotato.tlinkserver.domain.user.constant;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import java.util.Arrays;
 import lombok.Getter;
+import org.cotato.tlinkserver.global.exception.TLinkException;
+import org.cotato.tlinkserver.global.message.ErrorMessage;
 
 @Getter
 public enum Role {
@@ -12,5 +16,15 @@ public enum Role {
 
 	Role(String inKorean) {
 		this.inKorean = inKorean;
+	}
+
+	@JsonCreator
+	public static Role from(final String input) {
+		return Arrays.stream(Role.values())
+				.filter(role -> role.inKorean.equals(input))
+				.findFirst()
+				.orElseThrow(
+						() -> new TLinkException(ErrorMessage.BAD_REQUEST)
+				);
 	}
 }
