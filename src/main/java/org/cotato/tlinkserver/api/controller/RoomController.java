dfafa34ff1e5f91ccf51f98581847ddac09aa034
Yeongchan Kim <hades1@kakao.com>
2025-01-30 21:54:55 +0900

    Refactor [#52] 잘못된 반환 메세지 수정

--- src/main/java/org/cotato/tlinkserver/api/controller/RoomController.java
+++ src/main/java/org/cotato/tlinkserver/api/controller/RoomController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,7 +45,7 @@ public class RoomController {
 		@RequestBody RoomRequest roomRequest) {
 		Long userId = 1L;
 		roomFacade.modifyRoom(userId, roomId, roomRequest);
-		return ApiResponseUtil.success(SuccessMessage.DELETED);
+		return ApiResponseUtil.success(SuccessMessage.MODIFIED);
 	}
 
 	@DeleteMapping("/{roomId}")
