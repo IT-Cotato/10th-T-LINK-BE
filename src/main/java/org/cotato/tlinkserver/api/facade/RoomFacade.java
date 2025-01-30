d376495ad45419bc52868e2a065702c0be8eded2
Yeongchan Kim <hades1@kakao.com>
2025-01-30 19:26:01 +0900

    Refactor [#52] 과외방 목록 조회 코드 리팩토링

--- src/main/java/org/cotato/tlinkserver/api/facade/RoomFacade.java
+++ src/main/java/org/cotato/tlinkserver/api/facade/RoomFacade.java
@@ -1,17 +1,14 @@
 package org.cotato.tlinkserver.api.facade;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.cotato.tlinkserver.domain.room.Room;
 import org.cotato.tlinkserver.domain.room.application.RegistrationService;
 import org.cotato.tlinkserver.domain.room.application.RoomService;
+import org.cotato.tlinkserver.domain.room.application.dto.response.RoomDataResponse;
 import org.cotato.tlinkserver.domain.room.application.dto.response.RoomResponse;
 import org.cotato.tlinkserver.domain.room.application.dto.response.RoomsResponse;
-import org.cotato.tlinkserver.domain.user.User;
 import org.springframework.stereotype.Component;
 
-import jakarta.persistence.Tuple;
 import lombok.RequiredArgsConstructor;
 
 @Component
@@ -21,24 +18,11 @@ public class RoomFacade {
 	private final RoomService roomService;
 	private final RegistrationService registrationService;
 
-	public RoomsResponse getRoomsAndOpponents(Long userId) {
-		List<Tuple> roomsAndNames = registrationService.getRooms(userId);
-		List<User> opponents = roomsAndNames.stream()
-			.map(entry -> registrationService.getOpponent(entry.get(0, Room.class).getId(), userId))
+	public RoomsResponse getRooms(final Long userId) {
+		List<RoomDataResponse> roomData = registrationService.getRooms(userId);
+		List<RoomResponse> rooms = roomData.stream()
+			.map(r -> RoomResponse.from(r.room(), r.roomName(), r.user()))
 			.toList();
-
-		int size = roomsAndNames.size();
-		List<RoomResponse> rooms = new ArrayList<>();
-
-		for (int i=0; i<size; i++) {
-			RoomResponse room = RoomResponse.from(
-				roomsAndNames.get(i).get(0, Room.class),
-				roomsAndNames.get(i).get(1, String.class),
-				opponents.get(i)
-			);
-
-			rooms.add(room);
-		}
 
 		return RoomsResponse.from(rooms);
 	}
