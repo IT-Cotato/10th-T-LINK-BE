48ee417d845493ee0cd94ef70dbf7fe3ddbe2ef5
Yeongchan Kim <hades1@kakao.com>
2025-01-30 19:40:14 +0900

    Feat [#52] 과외방 수정을 위한 상세 조회 구현

--- src/main/java/org/cotato/tlinkserver/domain/room/application/RegistrationService.java
+++ src/main/java/org/cotato/tlinkserver/domain/room/application/RegistrationService.java
@@ -2,7 +2,9 @@ package org.cotato.tlinkserver.domain.room.application;
 
 import java.util.List;
 
+import org.cotato.tlinkserver.domain.room.Registration;
 import org.cotato.tlinkserver.domain.room.application.dto.response.RoomDataResponse;
+import org.cotato.tlinkserver.domain.room.application.dto.response.RoomModifyResponse;
 import org.cotato.tlinkserver.domain.room.infra.repository.RegistrationRepository;
 import org.springframework.stereotype.Service;
 
@@ -22,6 +24,27 @@ public class RegistrationService {
 		return registrationRepository.findAllByUserId(userId).stream()
 			.map(r -> RoomDataResponse.from(r.getRoom(), r.getName(), r.getUser()))
 			.toList();
+	}
+
+	public RoomModifyResponse getRoomModify(final Long userId, final Long roomId) {
+		List<Registration> registrations = registrationRepository.findAllByRoomId(roomId);
+
+		Registration teacherRegistration = registrations.stream()
+			.filter(r -> r.getUser().getId().equals(userId))
+			.findFirst()
+			.orElseThrow();
+		Registration studentRegistration = registrations.stream()
+			.filter(r -> !r.getUser().getId().equals(userId))
+			.findFirst()
+			.orElseThrow();
+
+		return RoomModifyResponse.from(
+			teacherRegistration.getRoom(),
+			teacherRegistration.getName(),
+			teacherRegistration,
+			studentRegistration
+		);
+
 	}
 
 }
