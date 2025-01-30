4ca50b2ab5d67e3238b49a0ffa9a14142f7768e6
Yeongchan Kim <hades1@kakao.com>
2025-01-30 19:40:14 +0900

    Feat [#52] 과외방 수정을 위한 상세 조회 구현

--- src/main/java/org/cotato/tlinkserver/domain/room/infra/repository/RegistrationRepository.java
+++ src/main/java/org/cotato/tlinkserver/domain/room/infra/repository/RegistrationRepository.java
@@ -15,4 +15,7 @@ public interface RegistrationRepository extends JpaRepository<Registration, Long
 	List<Registration> findRegistrationsByRoomId(Long roomId);
 
 
+	@Query("SELECT r FROM Registration r WHERE r.room.id = :roomId")
+	List<Registration> findAllByRoomId(@Param("roomId") Long roomId);
+
 }
