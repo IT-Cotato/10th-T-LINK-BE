6c22de2a02c072ab49e419b7d8e15b2ede2bab28
Yeongchan Kim <hades1@kakao.com>
2025-01-30 19:26:01 +0900

    Refactor [#52] 과외방 목록 조회 코드 리팩토링

--- src/main/java/org/cotato/tlinkserver/domain/room/application/dto/response/RoomDataResponse.java
+++ src/main/java/org/cotato/tlinkserver/domain/room/application/dto/response/RoomDataResponse.java
@@ -5,8 +5,8 @@ import org.cotato.tlinkserver.domain.room.Room;
 import org.cotato.tlinkserver.domain.user.User;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
-import software.amazon.awssdk.annotations.NotNull;
 
 @Builder
 public record RoomDataResponse
