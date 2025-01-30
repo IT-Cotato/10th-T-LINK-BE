f4e7f7f4bbc3fa865f5c9736e4114951296f12f1
Yeongchan Kim <hades1@kakao.com>
2025-01-30 19:57:28 +0900

    Feat [#52] 과외방 삭제 구현

--- src/main/java/org/cotato/tlinkserver/domain/room/application/RoomService.java
+++ src/main/java/org/cotato/tlinkserver/domain/room/application/RoomService.java
@@ -1,12 +1,7 @@
 package org.cotato.tlinkserver.domain.room.application;
 
-import org.cotato.tlinkserver.domain.room.Registration;
 import org.cotato.tlinkserver.domain.room.Room;
 import org.cotato.tlinkserver.domain.room.infra.repository.RoomRepository;
-import org.cotato.tlinkserver.domain.user.User;
-import org.cotato.tlinkserver.domain.user.constant.Role;
-import org.cotato.tlinkserver.global.exception.NotFoundException;
-import org.cotato.tlinkserver.global.message.ErrorMessage;
 import org.springframework.stereotype.Service;
 
 import lombok.AccessLevel;
@@ -41,21 +36,4 @@ public class RoomService {
 		return roomRepository.findByShareCode(shareCode);
 	}
 
-	public Long saveRoom(final Room room) {
-		Room save = roomRepository.save(room);
-		return save.getId();
-	}
-
-	public void removeRoom(final Long id) {
-		roomRepository.deleteById(id);
-	}
-
-	public void deleteRoom(final Long roomId) {
-		roomRepository.deleteById(roomId);
-	}
-
-	public Long saveRoom(Room room) {
-		Room save = roomRepository.save(room);
-		return save.getId();
-	}
 }
