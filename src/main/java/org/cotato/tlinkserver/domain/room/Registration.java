54a79c99bb5933610481212e7b1bc02d58dccfc0
Yeongchan Kim <hades1@kakao.com>
2025-01-30 22:47:41 +0900

    Refactor [#52] Registration 엔티티가 자체 기본 키를 갖도록 변경

--- src/main/java/org/cotato/tlinkserver/domain/room/Registration.java
+++ src/main/java/org/cotato/tlinkserver/domain/room/Registration.java
@@ -5,6 +5,8 @@ import org.cotato.tlinkserver.domain.user.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -23,11 +25,14 @@ import lombok.Setter;
 public class Registration {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "registration_id", updatable = false)
+	private Long id;
+
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "user_id")
 	private User user;
 
-	@Id
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "room_id")
 	private Room room;
