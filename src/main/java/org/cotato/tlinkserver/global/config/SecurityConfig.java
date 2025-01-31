d31f0531bb07d951c14d23fa2a5f21f9c900e9d2
chyun <och2510@gmail.com>
2025-02-01 01:16:22 +0900

    feat [#48] JWT 인증/인가 필터 추가

--- src/main/java/org/cotato/tlinkserver/global/config/SecurityConfig.java
+++ src/main/java/org/cotato/tlinkserver/global/config/SecurityConfig.java
@@ -1,15 +1,29 @@
 package org.cotato.tlinkserver.global.config;
 
+import lombok.RequiredArgsConstructor;
+import org.cotato.tlinkserver.auth.filter.JwtAuthenticationFilter;
+import org.cotato.tlinkserver.auth.jwt.CustomAccessDeniedHandler;
+import org.cotato.tlinkserver.auth.jwt.CustomJwtAuthenticationEntryPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
+@RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final CustomJwtAuthenticationEntryPoint customJwtAuthenticationEntryPoint;
+    private final CustomAccessDeniedHandler customAccessDeniedHandler;
+
+    private static final String[] AUTH_WHITELIST = {
+            "/*", "/**"
+    };
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -17,12 +31,23 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests((authorizeRequests) ->
-                        authorizeRequests
-                                .requestMatchers("/", "/**").permitAll()
-                ).cors(cors -> cors
+                .sessionManagement(session -> {
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                })
+                .cors(cors -> cors
                         .configurationSource(CorsConfig.corsConfigurationSource())
-                );
+                )
+                .exceptionHandling(exception ->
+                {
+                    exception.authenticationEntryPoint(customJwtAuthenticationEntryPoint);
+                    exception.accessDeniedHandler(customAccessDeniedHandler);
+                });
+
+        http.authorizeHttpRequests(auth -> {
+                    auth.requestMatchers(AUTH_WHITELIST).permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
