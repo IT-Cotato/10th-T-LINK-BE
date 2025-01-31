d4614e6bf50eb8300c9adebe36ccc683ad07173d
chyun <och2510@gmail.com>
2025-02-01 01:55:01 +0900

    feat [#48] 카카오 회원가입 AccessToken 발급 추가

--- src/main/java/org/cotato/tlinkserver/api/controller/AuthKakaoController.java
+++ src/main/java/org/cotato/tlinkserver/api/controller/AuthKakaoController.java
@@ -2,14 +2,18 @@ package org.cotato.tlinkserver.api.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.cotato.tlinkserver.api.dto.request.KakaoSignUpRequest;
+import org.cotato.tlinkserver.api.dto.response.KakaoSignUpResponse;
 import org.cotato.tlinkserver.api.facade.AuthKakaoFacade;
 import org.cotato.tlinkserver.api.facade.dto.request.KakaoSignUpDTO;
+import org.cotato.tlinkserver.global.common.BaseResponse;
 import org.cotato.tlinkserver.global.message.SuccessMessage;
 import org.cotato.tlinkserver.global.util.ApiResponseUtil;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,11 +23,18 @@ public class AuthKakaoController {
 
     private final AuthKakaoFacade authKakaoFacade;
 
-    @GetMapping("/signup")
-    public ResponseEntity<?> callback(
+    @GetMapping("/callback")
+    public ResponseEntity<BaseResponse<?>> callback(
+            @RequestParam String code
+    ) {
+        return ApiResponseUtil.success(SuccessMessage.SUCCESS, code);
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<BaseResponse<?>> signUp(
             @RequestBody KakaoSignUpRequest kakaoSignUpRequest
     ) {
-        authKakaoFacade.kakaoSignUp(KakaoSignUpDTO.from(kakaoSignUpRequest));
-        return ApiResponseUtil.success(SuccessMessage.SUCCESS);
+        final String accessToken = authKakaoFacade.kakaoSignUp(KakaoSignUpDTO.from(kakaoSignUpRequest));
+        return ApiResponseUtil.success(SuccessMessage.SUCCESS, KakaoSignUpResponse.from(accessToken));
     }
 }
