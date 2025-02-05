de8966091579798bd4fbd828891a4f41ae32af95
chyun <och2510@gmail.com>
2025-02-06 01:23:59 +0900

    refactor [#58] 성별 값 남자/여자 -> 남성/여성 변경

--- src/main/java/org/cotato/tlinkserver/domain/user/constant/Gender.java
+++ src/main/java/org/cotato/tlinkserver/domain/user/constant/Gender.java
@@ -8,8 +8,8 @@ import org.cotato.tlinkserver.global.message.ErrorMessage;
 
 @Getter
 public enum Gender {
-	MALE("남자"),
-	FEMALE("여자");
+	MALE("남성"),
+	FEMALE("여성");
 
 	private final String inKorean;
 
