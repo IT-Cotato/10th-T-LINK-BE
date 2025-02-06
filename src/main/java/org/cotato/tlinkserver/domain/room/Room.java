c5b021143357d883d123a142fae7ec3d373c1473
Yeongchan Kim <hades1@kakao.com>
2025-02-06 23:03:08 +0900

    Refactor [#52] 초기에 입력받지 않는 애트리뷰트 nullable 허용

--- src/main/java/org/cotato/tlinkserver/domain/room/Room.java
+++ src/main/java/org/cotato/tlinkserver/domain/room/Room.java
@@ -52,16 +52,16 @@ public class Room {
 	@JoinColumn(name = "account_bank_id")
 	private Bank bank;
 
-	@Column(name = "account_number", nullable = false, length = 20)
+	@Column(name = "account_number", length = 20)
 	private String accountNumber;
 
-	@Column(name = "deposit_at", nullable = false)
+	@Column(name = "deposit_at")
 	private int depositAt;
 
-	@Column(name = "deposit_amount", nullable = false)
+	@Column(name = "deposit_amount")
 	private int depositAmount;
 
-	@Column(name = "share_code", nullable = false, length = 250)
+	@Column(name = "share_code", length = 250)
 	private String shareCode;
 
 	@OneToMany(mappedBy = "room", cascade = CascadeType.ALL, orphanRemoval = true)
