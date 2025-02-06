da41d5aee62f1797a162d83fc375ed53a13fa862
Yeongchan Kim <hades1@kakao.com>
2025-02-06 20:16:57 +0900

    Refactor [#52] 공유 코드로 입장 리팩토링

--- src/main/java/org/cotato/tlinkserver/domain/room/application/RoomService.java
+++ src/main/java/org/cotato/tlinkserver/domain/room/application/RoomService.java
@@ -1,10 +1,7 @@
 package org.cotato.tlinkserver.domain.room.application;
 
-import org.cotato.tlinkserver.domain.room.Registration;
 import org.cotato.tlinkserver.domain.room.Room;
 import org.cotato.tlinkserver.domain.room.infra.repository.RoomRepository;
-import org.cotato.tlinkserver.domain.user.User;
-import org.cotato.tlinkserver.domain.user.constant.Role;
 import org.cotato.tlinkserver.global.exception.NotFoundException;
 import org.cotato.tlinkserver.global.message.ErrorMessage;
 import org.springframework.stereotype.Service;
