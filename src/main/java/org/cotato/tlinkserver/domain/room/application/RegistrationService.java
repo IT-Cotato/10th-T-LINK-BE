5645bcaf4fbccea93695418059db6d9692217849
Yeongchan Kim <hades1@kakao.com>
2025-02-06 19:46:59 +0900

    Refactor [#52] RegistrationRepository에서 Optional 사용

--- src/main/java/org/cotato/tlinkserver/domain/room/application/RegistrationService.java
+++ src/main/java/org/cotato/tlinkserver/domain/room/application/RegistrationService.java
@@ -3,10 +3,11 @@ package org.cotato.tlinkserver.domain.room.application;
 import java.util.List;
 
 import org.cotato.tlinkserver.domain.room.Registration;
-import org.cotato.tlinkserver.domain.room.application.dto.response.RoomDataResponse;
 import org.cotato.tlinkserver.domain.room.application.dto.response.RoomModifyResponse;
 import org.cotato.tlinkserver.domain.room.infra.repository.RegistrationRepository;
 import org.cotato.tlinkserver.domain.user.constant.Role;
+import org.cotato.tlinkserver.global.exception.NotFoundException;
+import org.cotato.tlinkserver.global.message.ErrorMessage;
 import org.springframework.stereotype.Service;
 
 
@@ -22,21 +23,17 @@ public class RegistrationService {
 	private final RegistrationRepository registrationRepository;
 
 	public Registration getRegistration(final Long roomId, final Role role) {
-		return registrationRepository.findRegistrationByRoomIdAndRole(roomId, role);
+		return registrationRepository.findRegistrationByRoomIdAndRole(roomId, role)
+			.orElseThrow(() -> new NotFoundException(ErrorMessage.NOT_FOUND));
 	}
 
 	public Registration getRegistration(final Long userId, final Long roomId) {
-		return registrationRepository.findRegistrationByUserIdAndRoomId(userId, roomId);
+		return registrationRepository.findRegistrationByUserIdAndRoomId(userId, roomId)
+			.orElseThrow(() -> new NotFoundException(ErrorMessage.NOT_FOUND));
 	}
 
 	public List<Registration> getRegistrations(final Long userId) {
 		return registrationRepository.findRegistrationsByUserId(userId);
-	}
-
-	public List<RoomDataResponse> getRooms(final Long userId) {
-		return registrationRepository.findRegistrationsByUserId(userId).stream()
-			.map(RoomDataResponse::from)
-			.toList();
 	}
 
 	public RoomModifyResponse getRoomModify(final Long roomId) {
--- src/main/java/org/cotato/tlinkserver/domain/room/infra/repository/RegistrationRepository.java
+++ src/main/java/org/cotato/tlinkserver/domain/room/infra/repository/RegistrationRepository.java
@@ -1,6 +1,7 @@
 package org.cotato.tlinkserver.domain.room.infra.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.cotato.tlinkserver.domain.room.Registration;
 import org.cotato.tlinkserver.domain.user.constant.Role;
@@ -8,8 +9,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RegistrationRepository extends JpaRepository<Registration, Long> {
 
-	Registration findRegistrationByUserIdAndRoomId(Long userId, Long roomId);
-	Registration findRegistrationByRoomIdAndRole(Long roomId, Role role);
+	Optional<Registration> findRegistrationByUserIdAndRoomId(Long userId, Long roomId);
+	Optional<Registration> findRegistrationByRoomIdAndRole(Long roomId, Role role);
 	List<Registration> findRegistrationsByUserId(Long userId);
 	List<Registration> findRegistrationsByRoomId(Long roomId);
 
