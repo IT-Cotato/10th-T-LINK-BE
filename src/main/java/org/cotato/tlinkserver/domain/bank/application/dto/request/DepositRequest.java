52991b48c3410e4a3d27e0b964ad6d69925849b3
Yeongchan Kim <hades1@kakao.com>
2025-02-07 16:50:13 +0900

    Fix [#45] 입금 정보 연관 관계 매핑 누락 수정

--- src/main/java/org/cotato/tlinkserver/domain/bank/application/dto/request/DepositRequest.java
+++ src/main/java/org/cotato/tlinkserver/domain/bank/application/dto/request/DepositRequest.java
@@ -1,7 +1,13 @@
 package org.cotato.tlinkserver.domain.bank.application.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record DepositRequest
 	(
+		@NotNull
+		Long bankId,
+		@NotBlank
 		String accountNumber,
 		int depositAt,
 		int depositAmount
