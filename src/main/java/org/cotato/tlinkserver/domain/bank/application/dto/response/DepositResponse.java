86b26b3f99d83da9c6861d738fc78ad8c689455b
Yeongchan Kim <hades1@kakao.com>
2025-02-07 19:09:47 +0900

    Feat [#45] 입금 정보가 반환이 안된 경우 처리

--- src/main/java/org/cotato/tlinkserver/domain/bank/application/dto/response/DepositResponse.java
+++ src/main/java/org/cotato/tlinkserver/domain/bank/application/dto/response/DepositResponse.java
@@ -2,6 +2,9 @@ package org.cotato.tlinkserver.domain.bank.application.dto.response;
 
 import org.cotato.tlinkserver.domain.room.Room;
 
+import lombok.Builder;
+
+@Builder
 public record DepositResponse
 	(
 		String accountNumber,
@@ -11,6 +14,10 @@ public record DepositResponse
 	)
 {
 	public static DepositResponse from(final Room room) {
+		if (room.getBank() == null) {
+			return DepositResponse.builder()
+				.build();
+		}
 		return new DepositResponse(
 			room.getAccountNumber(),
 			room.getBank().getName(),
