a54117321b68b8b488ea4fcac12ac0e5635042e8
Yeongchan Kim <hades1@kakao.com>
2025-02-07 20:32:07 +0900

    Fix [#52] Merge 시 미반영 문제 해결

--- src/main/java/org/cotato/tlinkserver/domain/room/application/RoomService.java
+++ src/main/java/org/cotato/tlinkserver/domain/room/application/RoomService.java
@@ -1,10 +1,7 @@
 package org.cotato.tlinkserver.domain.room.application;
 
-import org.cotato.tlinkserver.domain.room.Registration;
 import org.cotato.tlinkserver.domain.room.Room;
 import org.cotato.tlinkserver.domain.room.infra.repository.RoomRepository;
-import org.cotato.tlinkserver.domain.user.User;
-import org.cotato.tlinkserver.domain.user.constant.Role;
 import org.cotato.tlinkserver.global.exception.NotFoundException;
 import org.cotato.tlinkserver.global.message.ErrorMessage;
 import org.springframework.stereotype.Service;
@@ -26,19 +23,6 @@ public class RoomService {
 
 	public Room getRoom(final String shareCode) {
 		return roomRepository.findByShareCode(shareCode).orElseThrow(() -> new NotFoundException(ErrorMessage.NOT_FOUND));
-	}
-
-	public Long saveRoom(final Room room) {
-		Room save = roomRepository.save(room);
-		return save.getId();
-	}
-
-	public void removeRoom(final Long id) {
-		roomRepository.deleteById(id);
-	}
-
-	public Room getRoomByShareCode(final String shareCode) {
-		return roomRepository.findByShareCode(shareCode);
 	}
 
 	public Long saveRoom(final Room room) {
