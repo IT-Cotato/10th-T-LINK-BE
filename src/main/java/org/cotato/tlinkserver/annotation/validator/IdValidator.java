4b9c5d91db550957edd94b2af9e7a23b5c46dc9f
Yeongchan Kim <hades1@kakao.com>
2025-02-07 14:28:44 +0900

    Feat [#61] 객체의 ID를 검증하는 Annotation 및 Validator 생성

---
+++ src/main/java/org/cotato/tlinkserver/annotation/validator/IdValidator.java
@@ -0,0 +1,19 @@
+package org.cotato.tlinkserver.annotation.validator;
+
+import org.cotato.tlinkserver.annotation.IdValidation;
+import org.cotato.tlinkserver.global.exception.BadRequestException;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class IdValidator implements ConstraintValidator<IdValidation, Long> {
+
+	@Override
+	public boolean isValid(Long value, ConstraintValidatorContext context) {
+		if (value > 0) {
+			return true;
+		}
+		throw BadRequestException.wrong();
+	}
+
+}
