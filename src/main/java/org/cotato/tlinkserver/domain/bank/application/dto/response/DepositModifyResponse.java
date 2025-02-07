9417d973a37dff9a70c763d96b757e24b0d3bc60
Yeongchan Kim <hades1@kakao.com>
2025-02-07 19:28:23 +0900

    Feat [#45] 입금 정보 수정을 위한 정보 조회 시 입력이 안된 경우 처리

--- src/main/java/org/cotato/tlinkserver/domain/bank/application/dto/response/DepositModifyResponse.java
+++ src/main/java/org/cotato/tlinkserver/domain/bank/application/dto/response/DepositModifyResponse.java
@@ -2,6 +2,9 @@ package org.cotato.tlinkserver.domain.bank.application.dto.response;
 
 import org.cotato.tlinkserver.domain.room.Room;
 
+import lombok.Builder;
+
+@Builder
 public record DepositModifyResponse
 	(
 		String accountNumber,
@@ -12,6 +15,10 @@ public record DepositModifyResponse
 	)
 {
 	public static DepositModifyResponse from(final Room room) {
+		if (room.getBank() == null) {
+			return DepositModifyResponse.builder()
+				.build();
+		}
 		return new DepositModifyResponse(
 			room.getAccountNumber(),
 			room.getBank().getId(),
