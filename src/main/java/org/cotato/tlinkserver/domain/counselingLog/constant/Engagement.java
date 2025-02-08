a8d347c4b458dfdd496bdce24cff13613418ea46
Yeongchan Kim <hades1@kakao.com>
2025-02-08 12:41:19 +0900

    Refactor [#44] 상담 일지 생성 리팩토링

--- src/main/java/org/cotato/tlinkserver/domain/counselingLog/constant/Engagement.java
+++ src/main/java/org/cotato/tlinkserver/domain/counselingLog/constant/Engagement.java
@@ -1,5 +1,12 @@
 package org.cotato.tlinkserver.domain.counselingLog.constant;
 
+import java.util.Arrays;
+
+import org.cotato.tlinkserver.global.exception.TLinkException;
+import org.cotato.tlinkserver.global.message.ErrorMessage;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 import lombok.Getter;
 
 @Getter
@@ -12,5 +19,15 @@ public enum Engagement {
 
 	Engagement(String inKorean) {
 		this.inKorean = inKorean;
+	}
+
+	@JsonCreator
+	public static Engagement from(final String input) {
+		return Arrays.stream(Engagement.values())
+			.filter(engagement -> engagement.inKorean.equals(input))
+			.findFirst()
+			.orElseThrow(
+				() -> new TLinkException(ErrorMessage.BAD_REQUEST)
+			);
 	}
 }
