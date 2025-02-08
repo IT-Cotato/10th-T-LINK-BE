30397f4c2b02e41ee464c964859b1d79e06eb725
chyun <och2510@gmail.com>
2025-02-08 15:51:26 +0900

    feat [#69] 배경 색깔 정보 추가 제공

--- src/main/java/org/cotato/tlinkserver/api/dto/response/UserProfileResponse.java
+++ src/main/java/org/cotato/tlinkserver/api/dto/response/UserProfileResponse.java
@@ -8,6 +8,7 @@ public record UserProfileResponse(
         String statusMessage,
         String phoneNumber,
         String profileImageUrl,
+        String backgroundColor,
         Role role
 ) {
     public static UserProfileResponse from(UserProfileDTO userProfileDTO) {
@@ -16,6 +17,7 @@ public record UserProfileResponse(
                 userProfileDTO.statusMessage(),
                 userProfileDTO.phoneNumber(),
                 userProfileDTO.profileImageUrl(),
+                userProfileDTO.backgroundColor(),
                 userProfileDTO.role()
         );
     }
