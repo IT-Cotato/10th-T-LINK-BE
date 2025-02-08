dba0a7e45f674909e65622933d11f3fb3e9f8cbd
Yeongchan Kim <hades1@kakao.com>
2025-02-08 21:16:41 +0900

    Refactor [#43] 코드 변경으로 인한 테스트 케이스 삭제

--- src/main/java/org/cotato/tlinkserver/api/controller/FileTestController.java
+++ src/main/java/org/cotato/tlinkserver/api/controller/FileTestController.java
@@ -1,21 +1,16 @@
 package org.cotato.tlinkserver.api.controller;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 
-import org.cotato.tlinkserver.global.message.SuccessMessage;
-import org.cotato.tlinkserver.global.util.ApiResponseUtil;
 import org.cotato.tlinkserver.global.util.S3FileHandler;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import io.awspring.cloud.s3.S3Resource;
 import lombok.RequiredArgsConstructor;
@@ -27,14 +22,6 @@ import lombok.extern.slf4j.Slf4j;
 public class FileTestController {
 
 	private final S3FileHandler s3FileHandler;
-
-	@PostMapping("/upload")
-	public ResponseEntity<?> uploadFiles(@RequestPart(value = "file") List<MultipartFile> multipartFiles) throws
-		IOException {
-		List<String> list = Arrays.asList("aa", "bb", "cc");
-		s3FileHandler.uploadFiles(multipartFiles, list);
-		return ApiResponseUtil.success(SuccessMessage.SUCCESS);
-	}
 
 	@PostMapping("/download")
 	public ResponseEntity<?> downloadFile() {
