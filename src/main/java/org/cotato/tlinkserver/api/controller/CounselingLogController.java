fd7791693cfbc1b37b628602ac4d1173fe8b1f3f
Yeongchan Kim <hades1@kakao.com>
2025-02-08 12:23:16 +0900

    Refactor [#44] ApiResponseUtil 적용

--- src/main/java/org/cotato/tlinkserver/api/controller/CounselingLogController.java
+++ src/main/java/org/cotato/tlinkserver/api/controller/CounselingLogController.java
@@ -1,12 +1,12 @@
 package org.cotato.tlinkserver.api.controller;
 
-import org.cotato.tlinkserver.api.dto.response.DataResponse;
 import org.cotato.tlinkserver.api.facade.CounselingLogFacade;
 import org.cotato.tlinkserver.domain.counselingLog.application.dto.request.CounselingLogSaveRequest;
 import org.cotato.tlinkserver.domain.counselingLog.application.dto.response.CounselingLogDetailResponse;
 import org.cotato.tlinkserver.domain.counselingLog.application.dto.response.CounselingLogsResponse;
-import org.cotato.tlinkserver.global.util.SuccessMessage;
-import org.springframework.http.HttpStatus;
+import org.cotato.tlinkserver.global.common.BaseResponse;
+import org.cotato.tlinkserver.global.message.SuccessMessage;
+import org.cotato.tlinkserver.global.util.ApiResponseUtil;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,35 +27,35 @@ public class CounselingLogController {
 	private final CounselingLogFacade counselingLogFacade;
 
 	@GetMapping
-	public ResponseEntity<DataResponse<CounselingLogsResponse>> getCounselingLogs(@PathVariable(value = "roomId") Long roomId) {
+	public ResponseEntity<BaseResponse<?>> getCounselingLogs(@PathVariable(value = "roomId") Long roomId) {
 		CounselingLogsResponse counselingLogs = counselingLogFacade.getCounselingLogs(roomId);
-		return ResponseEntity.ok(DataResponse.of(HttpStatus.OK, SuccessMessage.SUCCESS.getDetailMessage(), counselingLogs));
+		return ApiResponseUtil.success(SuccessMessage.SUCCESS, counselingLogs);
 	}
 
 	@PostMapping
-	public ResponseEntity<DataResponse<?>> saveCounselingLog(@PathVariable(value = "roomId") Long roomId,
+	public ResponseEntity<BaseResponse<?>> saveCounselingLog(@PathVariable(value = "roomId") Long roomId,
 		@RequestBody CounselingLogSaveRequest counselingLogSaveRequest) {
 		counselingLogFacade.saveCounselingLog(roomId, counselingLogSaveRequest);
-		return ResponseEntity.ok(DataResponse.of(HttpStatus.CREATED, SuccessMessage.CREATED.getDetailMessage(), null));
+		return ApiResponseUtil.success(SuccessMessage.CREATED);
 	}
 
 	@GetMapping("/{counselingLogId}")
-	public ResponseEntity<DataResponse<CounselingLogDetailResponse>> getCounselingLog(@PathVariable(value = "counselingLogId") Long counselingLogId) {
+	public ResponseEntity<BaseResponse<?>> getCounselingLog(@PathVariable(value = "counselingLogId") Long counselingLogId) {
 		CounselingLogDetailResponse counselingLogDetail = counselingLogFacade.getCounselingLog(counselingLogId);
-		return ResponseEntity.ok(DataResponse.of(HttpStatus.OK, SuccessMessage.SUCCESS.getDetailMessage(), counselingLogDetail));
+		return ApiResponseUtil.success(SuccessMessage.SUCCESS, counselingLogDetail);
 	}
 
 	@PutMapping("/{counselingLogId}")
-	public ResponseEntity<DataResponse<?>> modifyCounselingLog(@PathVariable(value = "counselingLogId") Long counselingLogId,
+	public ResponseEntity<BaseResponse<?>> modifyCounselingLog(@PathVariable(value = "counselingLogId") Long counselingLogId,
 		@RequestBody CounselingLogSaveRequest counselingLogSaveRequest) {
 		counselingLogFacade.modifyCounselingLog(counselingLogId, counselingLogSaveRequest);
-		return ResponseEntity.ok(DataResponse.of(HttpStatus.OK, SuccessMessage.MODIFIED.getDetailMessage(), null));
+		return ApiResponseUtil.success(SuccessMessage.MODIFIED);
 	}
 
 	@DeleteMapping("/{counselingLogId}")
-	public ResponseEntity<DataResponse<?>> removeCounselingLog(@PathVariable(value = "counselingLogId") Long counselingLogId) {
+	public ResponseEntity<BaseResponse<?>> removeCounselingLog(@PathVariable(value = "counselingLogId") Long counselingLogId) {
 		counselingLogFacade.removeCounselingLog(counselingLogId);
-		return ResponseEntity.ok(DataResponse.of(HttpStatus.OK, SuccessMessage.DELETED.getDetailMessage(), null));
+		return ApiResponseUtil.success(SuccessMessage.DELETED);
 	}
 
 }
