69e1eb6735241476b98f79b78f21fc70d731590c
chyun <och2510@gmail.com>
2025-02-08 15:38:34 +0900

    feat [#69] 프로필 조회 API 구현

--- src/main/java/org/cotato/tlinkserver/api/controller/UserController.java
+++ src/main/java/org/cotato/tlinkserver/api/controller/UserController.java
@@ -1,28 +1,34 @@
 package org.cotato.tlinkserver.api.controller;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.cotato.tlinkserver.annotation.Permission;
 import org.cotato.tlinkserver.annotation.UserId;
 import org.cotato.tlinkserver.api.dto.request.RewriteStatusMessageRequest;
 import org.cotato.tlinkserver.api.dto.response.MyPageInfoResponse;
+import org.cotato.tlinkserver.api.dto.response.UserProfileResponse;
 import org.cotato.tlinkserver.api.facade.UserFacade;
 import org.cotato.tlinkserver.api.facade.dto.request.RewriteStatusMessageDTO;
 import org.cotato.tlinkserver.api.facade.dto.response.MyPageInfoDTO;
+import org.cotato.tlinkserver.api.facade.dto.response.UserProfileDTO;
 import org.cotato.tlinkserver.domain.user.constant.Role;
 import org.cotato.tlinkserver.global.common.BaseResponse;
 import org.cotato.tlinkserver.global.message.SuccessMessage;
 import org.cotato.tlinkserver.global.util.ApiResponseUtil;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Validated
 @RequestMapping("/api/v1/user")
 public class UserController {
 
@@ -52,5 +58,14 @@ public class UserController {
     ) {
         MyPageInfoDTO myPageInfoDTO = userFacade.getMyPageInfo(userId);
         return ApiResponseUtil.success(SuccessMessage.SUCCESS, MyPageInfoResponse.from(myPageInfoDTO));
+    }
+
+    @Permission(role = {Role.STUDENT, Role.PARENT, Role.TEACHER})
+    @GetMapping("/{userId}/profile")
+    public ResponseEntity<BaseResponse<?>> viewProfile(
+            @PathVariable @NotNull Long userId
+    ) {
+        UserProfileDTO userProfileDTO = userFacade.getUserProfile(userId);
+        return ApiResponseUtil.success(SuccessMessage.SUCCESS, UserProfileResponse.from(userProfileDTO));
     }
 }
---
+++ src/main/java/org/cotato/tlinkserver/api/facade/dto/response/UserProfileDTO.java
@@ -0,0 +1,22 @@
+package org.cotato.tlinkserver.api.facade.dto.response;
+
+import org.cotato.tlinkserver.domain.user.User;
+import org.cotato.tlinkserver.domain.user.constant.Role;
+
+public record UserProfileDTO(
+        String username,
+        String statusMessage,
+        String phoneNumber,
+        String profileImageUrl,
+        Role role
+) {
+    public static UserProfileDTO from(final User user) {
+        return new UserProfileDTO(
+                user.getUsername(),
+                user.getStatusMessage(),
+                user.getPhoneNumber(),
+                user.getProfilePath(),
+                user.getRole()
+        );
+    }
+}
