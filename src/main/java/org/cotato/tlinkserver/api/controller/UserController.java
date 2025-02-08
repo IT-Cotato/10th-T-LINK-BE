ba8a1843c35e9116bdee989b40691bc8f3067b30
chyun <och2510@gmail.com>
2025-02-08 15:08:29 +0900

    feat [#67] 마이페이지 상태메세지 수정 API 구현

--- src/main/java/org/cotato/tlinkserver/api/controller/UserController.java
+++ src/main/java/org/cotato/tlinkserver/api/controller/UserController.java
@@ -1,10 +1,13 @@
 package org.cotato.tlinkserver.api.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.cotato.tlinkserver.annotation.Permission;
 import org.cotato.tlinkserver.annotation.UserId;
+import org.cotato.tlinkserver.api.dto.request.RewriteStatusMessageRequest;
 import org.cotato.tlinkserver.api.dto.response.MyPageInfoResponse;
 import org.cotato.tlinkserver.api.facade.UserFacade;
+import org.cotato.tlinkserver.api.facade.dto.request.RewriteStatusMessageDTO;
 import org.cotato.tlinkserver.api.facade.dto.response.MyPageInfoDTO;
 import org.cotato.tlinkserver.domain.user.constant.Role;
 import org.cotato.tlinkserver.global.common.BaseResponse;
@@ -13,6 +16,8 @@ import org.cotato.tlinkserver.global.util.ApiResponseUtil;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +32,16 @@ public class UserController {
     @DeleteMapping
     public ResponseEntity<BaseResponse<?>> deleteAccount(@UserId Long userId) {
         userFacade.deleteAccount(userId);
+        return ApiResponseUtil.success(SuccessMessage.SUCCESS);
+    }
+
+    @Permission(role = {Role.STUDENT, Role.PARENT, Role.TEACHER})
+    @PatchMapping("/mypage/statusMessage")
+    public ResponseEntity<BaseResponse<?>> rewriteStatusMessage(
+            @UserId Long userId,
+            @RequestBody @Valid RewriteStatusMessageRequest request
+    ) {
+        userFacade.rewriteStatusMessage(userId, RewriteStatusMessageDTO.from(request));
         return ApiResponseUtil.success(SuccessMessage.SUCCESS);
     }
 
