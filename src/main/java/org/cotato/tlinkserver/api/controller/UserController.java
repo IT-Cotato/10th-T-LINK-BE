f33ff9cd8fada4b731ea601fad3b8d16ca5f140f
chyun <och2510@gmail.com>
2025-02-08 14:15:30 +0900

    feat [#64] 마이페이지 조회 API 구현

--- src/main/java/org/cotato/tlinkserver/api/controller/UserController.java
+++ src/main/java/org/cotato/tlinkserver/api/controller/UserController.java
@@ -3,13 +3,16 @@ package org.cotato.tlinkserver.api.controller;
 import lombok.RequiredArgsConstructor;
 import org.cotato.tlinkserver.annotation.Permission;
 import org.cotato.tlinkserver.annotation.UserId;
+import org.cotato.tlinkserver.api.dto.response.MyPageInfoResponse;
 import org.cotato.tlinkserver.api.facade.UserFacade;
+import org.cotato.tlinkserver.api.facade.dto.response.MyPageInfoDTO;
 import org.cotato.tlinkserver.domain.user.constant.Role;
 import org.cotato.tlinkserver.global.common.BaseResponse;
 import org.cotato.tlinkserver.global.message.SuccessMessage;
 import org.cotato.tlinkserver.global.util.ApiResponseUtil;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +28,14 @@ public class UserController {
     public ResponseEntity<BaseResponse<?>> deleteAccount(@UserId Long userId) {
         userFacade.deleteAccount(userId);
         return ApiResponseUtil.success(SuccessMessage.SUCCESS);
+    }
+
+    @Permission(role = {Role.STUDENT, Role.PARENT, Role.TEACHER})
+    @GetMapping("/mypage")
+    public ResponseEntity<BaseResponse<?>> viewMyPage(
+            @UserId Long userId
+    ) {
+        MyPageInfoDTO myPageInfoDTO = userFacade.getMyPageInfo(userId);
+        return ApiResponseUtil.success(SuccessMessage.SUCCESS, MyPageInfoResponse.from(myPageInfoDTO));
     }
 }
