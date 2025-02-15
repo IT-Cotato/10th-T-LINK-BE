a32c746bcae460a5615975b608e0ae5859b66187
kych4n <hades1@kakao.com>
2025-02-15 16:22:24 +0900

    Refactor [#121] 초대창 변경으로 인한 제공 정보 수정

--- src/main/java/org/cotato/tlinkserver/api/dto/RoomJoinResponse.java
+++ src/main/java/org/cotato/tlinkserver/api/dto/RoomJoinResponse.java
@@ -1,11 +1,33 @@
 package org.cotato.tlinkserver.api.dto;
 
+import java.util.List;
+import lombok.Builder;
+import org.cotato.tlinkserver.api.dto.response.RoomLessonDayResponse;
+import org.cotato.tlinkserver.domain.room.Registration;
+import org.cotato.tlinkserver.domain.room.Room;
+import org.cotato.tlinkserver.domain.room.application.dto.response.OpponentResponse;
+import org.cotato.tlinkserver.domain.user.User;
+
+@Builder
 public record RoomJoinResponse
 	(
-		String teacherName
+			Long roomId,
+			String roomName,
+			String subject,
+			List<RoomLessonDayResponse> lessonDays,
+			OpponentResponse opponent
 	)
 {
-	public static RoomJoinResponse from(String teacherName) {
-		return new RoomJoinResponse(teacherName);
+	public static RoomJoinResponse from(Registration registration) {
+		Room room = registration.getRoom();
+		User opponent = registration.getUser();
+
+		return RoomJoinResponse.builder()
+				.roomId(room.getId())
+				.roomName(registration.getRoomName())
+				.subject(room.getSubject())
+				.lessonDays(room.getLessonDays().stream().map(lessonDay -> RoomLessonDayResponse.from(lessonDay.getLessonDay().getInKorean())).toList())
+				.opponent(OpponentResponse.from(opponent, opponent.getUsername()))
+				.build();
 	}
 }
