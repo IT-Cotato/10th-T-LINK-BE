09fed4fc5b71aa4a94d38df227f6617a6abc5d8f
kych4n <hades1@kakao.com>
2025-02-19 12:41:45 +0900

    Feat [#126] 시험 종류 추가

--- src/main/java/org/cotato/tlinkserver/domain/gradeStatistic/ExamBox.java
+++ src/main/java/org/cotato/tlinkserver/domain/gradeStatistic/ExamBox.java
@@ -4,6 +4,8 @@ import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -12,6 +14,7 @@ import jakarta.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -25,6 +28,7 @@ import org.cotato.tlinkserver.domain.room.Room;
 public class ExamBox {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "exam_box_id", updatable = false)
     private Long id;
 
@@ -37,6 +41,11 @@ public class ExamBox {
 
     @OneToMany(mappedBy = "examBox", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Exam> exams = new ArrayList<>();
+
+    @Builder
+    public ExamBox(String name) {
+        this.name = name;
+    }
 
     public void addExam(Exam exam) {
         exams.add(exam);
