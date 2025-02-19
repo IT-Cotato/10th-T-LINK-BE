c443370854b0881797a7d823e9b8b796af3a148c
kych4n <hades1@kakao.com>
2025-02-19 12:04:55 +0900

    Feat [#126] 시험 종류, 시험 엔티티 생성

---
+++ src/main/java/org/cotato/tlinkserver/domain/gradeStatistic/ExamBox.java
@@ -0,0 +1,46 @@
+package org.cotato.tlinkserver.domain.gradeStatistic;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.cotato.tlinkserver.domain.room.Room;
+
+@Entity
+@Table(name = "exam_boxes")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Setter
+public class ExamBox {
+
+    @Id
+    @Column(name = "exam_box_id", updatable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 30)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    @OneToMany(mappedBy = "examBox", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Exam> exams = new ArrayList<>();
+
+    public void addExam(Exam exam) {
+        exams.add(exam);
+        exam.setExamBox(this);
+    }
+
+}
