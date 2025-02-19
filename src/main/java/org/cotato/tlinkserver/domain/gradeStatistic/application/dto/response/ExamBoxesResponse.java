219460ef96d4ddae75246c09ef39890bd8f3e755
kych4n <hades1@kakao.com>
2025-02-19 13:02:10 +0900

    Feat [#126] 시험 종류 조회

---
+++ src/main/java/org/cotato/tlinkserver/domain/gradeStatistic/application/dto/response/ExamBoxesResponse.java
@@ -0,0 +1,13 @@
+package org.cotato.tlinkserver.domain.gradeStatistic.application.dto.response;
+
+import java.util.List;
+import lombok.Builder;
+
+@Builder
+public record ExamBoxesResponse(
+    List<ExamBoxResponse> examBox
+) {
+    public static ExamBoxesResponse from(List<ExamBoxResponse> examBox) {
+        return ExamBoxesResponse.builder().examBox(examBox).build();
+    }
+}
