adae7001b2f4d06a307c386419258f554c73eda1
kych4n <hades1@kakao.com>
2025-02-20 17:49:21 +0900

    Refactor [#128] 성적 조회에서 ID 추가

--- src/main/java/org/cotato/tlinkserver/domain/gradeStatistic/application/dto/response/ExamResponse.java
+++ src/main/java/org/cotato/tlinkserver/domain/gradeStatistic/application/dto/response/ExamResponse.java
@@ -5,11 +5,13 @@ import org.cotato.tlinkserver.domain.gradeStatistic.Exam;
 
 @Builder
 public record ExamResponse(
+    Long examId,
     String examName,
     Integer grade
 ) {
     public static ExamResponse from(Exam exam) {
         return ExamResponse.builder()
+                .examId(exam.getId())
                 .examName(exam.getName())
                 .grade(exam.getGrade())
                 .build();
