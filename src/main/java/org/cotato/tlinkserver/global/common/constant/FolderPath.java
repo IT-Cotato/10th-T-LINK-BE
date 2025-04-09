259156cd494c1ef7cdb5cf919dcc23454db11b8b
kych4n <hades1@kakao.com>
2025-04-09 23:38:15 +0900

    refactor [#136] S3에 저장하는 파일 경로 Enum으로 관리

---
+++ src/main/java/org/cotato/tlinkserver/global/common/constant/FolderPath.java
@@ -0,0 +1,20 @@
+package org.cotato.tlinkserver.global.common.constant;
+
+public enum FolderPath {
+    LECTURE("lecture"), HOMEWORK("homework");
+
+    private static final String DELIMITER = "/";
+    private final String path;
+
+    FolderPath(String path) {
+        this.path = path;
+    }
+
+    public static String generate(FolderPath folderPath, String key) {
+        return String.join(DELIMITER, folderPath.path(), key);
+    }
+
+    public String path() {
+        return this.path;
+    }
+}
