030cbdf83602ca083fcfa0f607a304fc297530a2
kych4n <hades1@kakao.com>
2025-05-10 00:25:18 +0900

    fix [#146] 엔티티 리스너 추가

--- src/main/java/org/cotato/tlinkserver/domain/lectureFile/LectureFileBox.java
+++ src/main/java/org/cotato/tlinkserver/domain/lectureFile/LectureFileBox.java
@@ -3,6 +3,7 @@ package org.cotato.tlinkserver.domain.lectureFile;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,10 +22,12 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.cotato.tlinkserver.domain.room.Room;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Table(name = "lecture_file_boxes")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 public class LectureFileBox {
@@ -47,8 +50,7 @@ public class LectureFileBox {
     private List<LectureFile> lectureFiles = new ArrayList<>();
 
     @Builder
-    public LectureFileBox(Room room, String name) {
-        this.room = room;
+    public LectureFileBox(String name) {
         this.name = name;
     }
 
