97c9b6268992f9319a67deaf2d812f5858ae2f97
kych4n <hades1@kakao.com>
2025-05-10 00:47:38 +0900

    fix [#146] 전화번호 중복 검증 로직 추가

--- src/main/java/org/cotato/tlinkserver/domain/user/infra/repository/UserRepository.java
+++ src/main/java/org/cotato/tlinkserver/domain/user/infra/repository/UserRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findBySocialIdAndProvider(String socialId, SocialProvider provider);
+
+    Optional<User> findByPhoneNumber(String phoneNumber);
 }
